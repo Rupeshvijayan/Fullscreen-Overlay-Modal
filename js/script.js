f36f3b1bd06ce852424a263aa0fde1ee2f0d7fee
Rupesh Vijayan <111982286+Rupeshvijayan@users.noreply.github.com>
2024-02-14 14:21:05 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -2,25 +2,15 @@ const modal = document.querySelector(".modal");
 const btnOpenModal = document.querySelector(".show-modal");
 const btnCloseModal = document.querySelector(".close-modal");
 
-btnOpenModal.addEventListener("click",function(){
+btnOpenModal.addEventListener("click", function() {
+    modal.style.animation = "slide-in 1.5s";
     modal.classList.remove('hidden');
-    modal.animate([
-        { transform: "translateX(-100%)"},
-        { transform: "translateX(0)"}
-    ], {
-        duration: 1500,
-    });
-})
+});
 
-btnCloseModal.addEventListener("click", function(){
-    setTimeout(function(){
+btnCloseModal.addEventListener("click", function() {
+    modal.style.animation = "slide-out 1.5s";
+    setTimeout(function() {
         modal.classList.add('hidden');
-    },1500)
-    
-    modal.animate([
-        { transform: "translateX(0)" },
-        { transform: "translateX(-100%)" }
-    ], {
-        duration: 1500,
-    });
-})
+    }, 1500);
+});
+
